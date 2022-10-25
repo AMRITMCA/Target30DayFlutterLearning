76069909dd873a88f0ab9cb6a96456a1ea5959fe
AMRITMCA <amritkumar0201999@gmail.com>
2022-10-25 23:25:08 +0530

    jokes

--- flutter/scafold/lib/main.dart
+++ flutter/scafold/lib/main.dart
@@ -4,7 +4,34 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var count = 0;
+  var jokes = [
+    'Learn programming to understand programming jokes.',
+    'Code never lies,comments sometimes do.',
+    'You are semicolons to my Statements',
+    'Programming is 1% writing code and 99% understanding why its not working',
+    'I told him I cound not open jar. He told me to download java',
+    'Comparing java and javascript is same as comparing car and carpet',
+    'Golden rule of programming - If it works dont touch it.'
+  ];
+  void increamentCount() {
+    setState(() {
+      if (count == jokes.length - 1) {
+        count = 0;
+      }
+      else{
+count = count + 1;
+      }
+      
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,17 +42,27 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.pink[50],
         ),
         body: Center(
-          child: Text('coders never quit'),
+          child: Text(
+            //'Count: $count',
+            '${jokes[count]}',
+            style: TextStyle(
+                fontSize: 40,
+                fontStyle: FontStyle.italic,
+                color: Colors.pink[800],
+                fontWeight: FontWeight.bold),
+          ),
         ),
-        floatingActionButton: FloatingActionButton(onPressed: () {
-          print('click');
-
-        },
-        child: Icon(Icons.add),
-        backgroundColor: Colors.pink[400],
+        floatingActionButton: FloatingActionButton(
+          onPressed: increamentCount,
+          child: Icon(
+            Icons.refresh,
+            size: 30,
+          ),
+          backgroundColor: Colors.pink[400],
         ),
-
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       ),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
